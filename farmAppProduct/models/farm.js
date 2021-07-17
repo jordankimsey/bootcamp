51ebc1e89c67198061d3dfe60bb5128b54c70055
Isaac Jordan kimsey <74661612+jordankimsey@users.noreply.github.com>
2021-07-16 22:14:42 -0400

    Add files via upload

---
+++ farmAppProduct/models/farm.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const Product = require('./product')
+
+const farmSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Farm must have a name!']
+    },
+    city: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, 'Email Required']  
+    },
+    //two way relationship to DB
+    //shows products available at specific farm
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+       } 
+    ]
+})
+//dont have to call next if using async function
+farmSchema.post('findOneAndDelete', async function (farm) {
+    if (farm.products.length) {
+        const res = await Product.deleteMany({ _id: { $in: farm.products } });
+        console.log(res);
+  }
+})
+
+const Farm = mongoose.model('Farm', farmSchema);
+module.exports = Farm;
+
